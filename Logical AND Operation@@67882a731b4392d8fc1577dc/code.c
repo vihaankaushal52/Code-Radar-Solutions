ff65d6dd6a42ce2696b2e8fa78a1216ee8bdf0a5
Vihaan Kaushal <vihaankaushal25@gmail.com>
2025-03-10 13:57:31 +0530

    Solution to Logical AND Operation

---
+++ Logical AND Operation@@67882a731b4392d8fc1577dc/code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    // Reading two space-separated integers
+    scanf("%d %d", &num1, &num2);
+
+    // Checking if both numbers are greater than zero
+    if (num1 > 0 && num2 > 0) {
+        printf("True\n");
+    } else {
+        printf("False\n");
+    }
+
+    return 0;
+}
