bc8e349d7a648aad0097bab76e67b65b6df3346a
Vihaan Kaushal <vihaankaushal25@gmail.com>
2025-03-10 13:54:17 +0530

    Solution to Check Equal To

--- Check Equal To@@67882a731b4392d8fc1577da/code.c
+++ Check Equal To@@67882a731b4392d8fc1577da/code.c
@@ -3,10 +3,9 @@
 int main() {
     int num1, num2;
 
-    // Reading two space-separated integers
+    
     scanf("%d %d", &num1, &num2);
 
-    // Checking if the first number is greater than or equal to the second
     if (num1 >= num2) {
         printf("True\n");
     } else {
